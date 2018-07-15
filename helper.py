f019ff9f9a059c6c5666f76cb27bb1810f907df8
Au Yong Min Hao <mauyong@moneylion.com>
2018-07-15 17:32:08 +0800

    Run games faster

--- helper.py
+++ helper.py
@@ -48,7 +48,7 @@ def run_game(game_name):
                 print("Reward: {}".format(reward))
                 print("Done: {}".format(reward))
                 print("Info: {}".format(info))
-                time.sleep(1)
+                time.sleep(0.2)
 
                 if done:
                     print("Episode {} finished after {} timesteps".format(i_episode, t+1))
