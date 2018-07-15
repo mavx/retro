692cc7688ff97449820c06ec0aea6ad2ad7d9b75
Au Yong Min Hao <mauyong@moneylion.com>
2018-07-15 15:49:54 +0800

    Try taxi

--- taxi.py
+++ taxi.py
@@ -1,6 +1,18 @@
 """
 Link: http://gym.openai.com/
 """
-
+import time
 import gym
+
 env = gym.make("Taxi-v2")
+observation = env.reset()
+for _ in range(1000):
+    env.render()
+    action = env.action_space.sample() # your agent here (this takes random actions)
+    observation, reward, done, info = env.step(action)
+    print("Observation", observation)
+    print("Reward", reward)
+    print("Done", done)
+    print("Info", info)
+    # break
+    time.sleep(1)
